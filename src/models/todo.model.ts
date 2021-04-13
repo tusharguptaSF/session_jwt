f6e4807f269fc5b5865d6fb3b7630ca9173662e9
Amisha <amisha.gupta@stackroute.in>
2021-04-13 14:55:37 +0530

    header in displayed

---
+++ src/models/todo.model.ts
@@ -0,0 +1,44 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  text: string;
+
+  @property({
+    type: 'date',
+  })
+  dateOfC?: string;
+
+  @property({
+    type: 'string',
+  })
+  status?: string;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
